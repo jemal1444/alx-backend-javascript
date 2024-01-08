960c84f51ed27791469f4eea1ef3a619d9867d9c
jemal1444 <jemal.eyasu@yahoo.com>
2024-01-08 21:36:54 +0300

    This is an ALX project on 0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,21 @@
-const getListStudents = () => [
-  {
+export default function getListStudents() {
+  const o1 = {
     id: 1,
     firstName: 'Guillaume',
     location: 'San Francisco',
-  },
-  {
+  };
+
+  const o2 = {
     id: 2,
     firstName: 'James',
     location: 'Columbia',
-  },
-  {
+  };
+
+  const o3 = {
     id: 5,
     firstName: 'Serena',
     location: 'San Francisco',
-  },
-];
+  };
 
-export default getListStudents;
+  return [o1, o2, o3];
+}
